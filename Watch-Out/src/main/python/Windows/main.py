927f89083bbb212369e3dddd31a0702e58aa4e63
OShu4 <corradoogalantini@gmail.com>
2022-05-16 18:13:31 +0200

    Update main.py

--- Watch-Out/src/main/python/Windows/main.py
+++ Watch-Out/src/main/python/Windows/main.py
@@ -52,28 +52,31 @@ def timer():
     global FIRED, isMENU, LOSE
     start = time.time()
     while True:
+        if isMENU or LOSE:
+            return
         if FIRED and not LOSE:
             end = time.time()
             if end-start < float(BESTSCORE) or BESTSCORE == 0.0:
                 FileManager.writeTO(float(end), start, "data")
             return
-        if isMENU or LOSE:
-            return
+
 
 
 def OWN_handle_bullet(ENEMY_PG):
     global OWN_bullets, ENEMY_NUMBER
+
     if OWN_bullets:
         OWN_bullets.y -= BULLET_VEL
         if ENEMY_PG.colliderect(OWN_bullets):
             OWN_bullets = NULL
             ENEMY_NUMBER = str(int(ENEMY_NUMBER)+1)
             draw_winner("hai vinto!", True, G)
-            return
+    return
 
 
 def ENEMY_handle_bullet(OWN_PG):
     global ENEMY_bullets, PG_HP
+
     if ENEMY_bullets:
         ENEMY_bullets.y += BULLET_VEL
         if OWN_PG.colliderect(ENEMY_bullets):
@@ -84,30 +87,39 @@ def ENEMY_handle_bullet(OWN_PG):
 
 
 def background_window(OWN_PG_img, OWN_PG, ENEMY_PG, ENEMY_PG_img, EXIT, MENU_MOUSE_POS):
+
     global ENEMY_bullets, CANFIRE, OWN_bullets, FIRED, ENEMY_NUMBER, PG_HP
     WIN.fill(W)
     WIN.blit(OWN_PG_img, (OWN_PG.x, OWN_PG.y))
     WIN.blit(ENEMY_PG_img, (ENEMY_PG.x, ENEMY_PG.y))
-    draw_text = get_font(18).render("LEVEL "+str(ENEMY_NUMBER), 1, B)
-    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, 55))
+
     draw_text = get_font(15).render(str(PG_HP), 1, B)
     WIN.blit(draw_text, (965, 55))
     WIN.blit(HP_img, (1000, 50))
+
+    draw_text = get_font(18).render("LEVEL "+str(ENEMY_NUMBER), 1, B)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, 55))
+
     if CANFIRE == True and FIRED == False:
         draw_text = get_font(100).render("Watch Out!", 1, B)
         WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
                              2, HEIGHT/2 - draw_text.get_height()/2))
+                             
     if OWN_bullets:
         pygame.draw.rect(WIN, B, OWN_bullets)
+
     if ENEMY_bullets:
         pygame.draw.rect(WIN, B, ENEMY_bullets)
+
     EXIT.changeColor(MENU_MOUSE_POS)
     EXIT.update(WIN)
+
     pygame.display.update()
 
 
 def changeLevel():
     global WIDTH, HEIGHT, ENEMY_NUMBER
+
     WIN.fill(W)
     draw_text1 = get_font(80).render("LEVEL "+str(ENEMY_NUMBER), 1, B)
     WIN.blit(draw_text1, (WIDTH/2 - draw_text1.get_width() /
@@ -119,22 +131,23 @@ def changeLevel():
 
 def draw_winner(text, go, color):
     global ENEMY_NUMBER, PG_HP
+
     WIN.fill(W)
     draw_text = get_font(100).render(text, 1, color)
     WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
                          2, HEIGHT/2 - draw_text.get_height()/2))
     pygame.display.update()
     pygame.time.delay(800)
-    if go:
-        if not (ENEMY_NUMBER == "5" or PG_HP == 0):
-            changeLevel()
-        play()
-    else:
+    if not go:
         main_menu()
+    if not (ENEMY_NUMBER == "5" or PG_HP == 0):
+        changeLevel()
+    play()
 
 
 def firetimer():
     global CANFIRE, DIE
+
     DIE = True
     T = random.uniform(1.5, 3)
     time.sleep(T)
@@ -149,16 +162,17 @@ def ENEMY_FIRE(i, ENEMY_PG):
     global ENEMY_bullets, CANFIRE, FIRED, LEVELDIFF, ENEMY_NUMBER, LOSE, isMENU, EASY_DIFF
     while CANFIRE == False:
         pass
-    if ENEMY_NUMBER != "0":
-        if EASY_DIFF:
-            time.sleep(0.33)
-        else:
-            time.sleep(LEVELDIFF[int(ENEMY_NUMBER)])
-        if CANFIRE and not FIRED and not isMENU:
-            LOSE = True
-            ENEMY_bullets = pygame.Rect(
-                ENEMY_PG.x + OWN_PG_H//2 - 5, ENEMY_PG.y + 40, 10, 5)
-            FIRED = True
+
+    if ENEMY_NUMBER == "0":
+        return
+    if EASY_DIFF:
+        time.sleep(0.33)
+    else:
+        time.sleep(LEVELDIFF[int(ENEMY_NUMBER)])
+    if CANFIRE and not FIRED and not isMENU:
+        LOSE = True
+        ENEMY_bullets = pygame.Rect(ENEMY_PG.x + OWN_PG_H//2 - 5, ENEMY_PG.y + 40, 10, 5)
+        FIRED = True
     return
 
 
@@ -176,9 +190,9 @@ def check_fire(OWN_PG):
         draw_winner("hai perso!", True, R)
     return
 
-
 def toMenu():
     global CANFIRE, ENEMY_NUMBER, isMENU
+
     if not CANFIRE:
         ENEMY_NUMBER = "0"
         isMENU = True
@@ -265,14 +279,40 @@ def change_difficult():
         EASY_DIFF = True
     return
 
+def set_level():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+        WIN.fill("white")
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        WIN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(WIN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
 
 def main_menu():
+
     pos = 0
     global BESTSCORE, EASY_DIFF
     score1 = get_font(20).render("Your best score:", 1, (255, 255, 0))
     BESTSCORE = FileManager.getScore()
     scoreStr = get_font(18).render(str(BESTSCORE), 1, (255, 255, 0))
+
     while True:
+
         WIN.blit(BG, (0, 0))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
@@ -292,15 +332,18 @@ def main_menu():
             30), base_color=W, hovering_color="#ddffd0")
         QUIT_BUTTON = Button(minImage, pos=(640, 560), text_input="QUIT", font=get_font(
             45), base_color=W, hovering_color="#ddffd0")
+
         WIN.blit(score1, (900, 270))
         WIN.blit(scoreStr, (1020, 305))
         WIN.blit(MENU_TEXT, MENU_RECT)
         WIN.blit(WATCH_OUT, TITLE_RECT)
+
         for button in [PLAY_BUTTON, CHANGE_DIFFICULT, QUIT_BUTTON, SELECT_LEVEL]:
             PLAY_BUTTON.changeColorArrow(pos, 0)
             if EASY_DIFF:
                 CHANGE_DIFFICULT.base_color = G
                 CHANGE_DIFFICULT.hovering_color = "#95c799"
+
             CHANGE_DIFFICULT.changeColorArrow(pos, 1)
             SELECT_LEVEL.changeColorArrow(pos, 2)
             QUIT_BUTTON.changeColorArrow(pos, 3)
@@ -317,7 +360,16 @@ def main_menu():
                 if CHANGE_DIFFICULT.checkForInput(MENU_MOUSE_POS):
                     change_difficult()
                 if SELECT_LEVEL.checkForInput(MENU_MOUSE_POS):
-                    print(FileManager.bin_to_str("fin"))
+                    if FileManager.bin_to_str("fin")!=" completed":
+                        print("yuuss")
+                        draw_text = get_font(20).render("Devi prima completare il gioco!", 1, R)
+                        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
+                             2, (HEIGHT/2 - draw_text.get_height()/2)+15))
+                        pygame.display.update()
+                        pygame.time.delay(400)
+                    else:
+                        set_level()
+
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
@@ -343,7 +395,7 @@ def main_menu():
                 if pos == 4:
                     pygame.quit()
                     sys.exit()
-        pygame.display.update()
 
+        pygame.display.update()
 
 main_menu()
